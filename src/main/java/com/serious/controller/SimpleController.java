52c1d558f332e1eaf25abd396a5d24cf7f5126d2
maduo <lovemd@vip.qq.com>
2017-01-04 14:47:12 +0800

    init

--- src/main/java/com/serious/controller/SimpleController.java
+++ src/main/java/com/serious/controller/SimpleController.java
@@ -4,17 +4,16 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by Serious on 2017/1/4.
  */
-@Controller
+@RestController
 @EnableAutoConfiguration
 public class SimpleController {
     @RequestMapping("/")
-    @ResponseBody
     String home() {
         return "Hello World!";
     }
-
 }
